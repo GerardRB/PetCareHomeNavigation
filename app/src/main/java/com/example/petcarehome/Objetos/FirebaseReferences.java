cfd10a12bcedc8a399c36afa6e522f5c9d687055
Gerard RB <ger.caligarii@gmail.com>
2020-09-23 19:21:29 -0500

    Dependencias firebase Storage

--- app/src/main/java/com/example/petcarehome/Objetos/FirebaseReferences.java
+++ app/src/main/java/com/example/petcarehome/Objetos/FirebaseReferences.java
@@ -1,7 +1,7 @@
 package com.example.petcarehome.Objetos;
 
 public class FirebaseReferences {
-    final public static String PETCARE_REFERENCE = "petCare";
+    final public static String PETCARE_REFERENCE = "reportes";
     final public static String REPORTEPERDIDA_REFERENCE = "reportePerdida";
     final public static String REPORTEENCONTRADA_REFERENCE = "reporteEncontrada";
     final public static String REPORTEADOPCION_REFERENCE = "reporteAdopcion";
