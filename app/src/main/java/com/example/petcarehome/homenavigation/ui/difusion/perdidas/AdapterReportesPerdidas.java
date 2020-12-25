54ffaf8eee38a71c69473a26ef8e2e302b7211d0
Gerard RB <ger.caligarii@gmail.com>
2020-12-25 15:40:05 -0600

    Se cambió Picasso por Glide en los reportes.

--- app/src/main/java/com/example/petcarehome/homenavigation/ui/difusion/perdidas/AdapterReportesPerdidas.java
+++ app/src/main/java/com/example/petcarehome/homenavigation/ui/difusion/perdidas/AdapterReportesPerdidas.java
@@ -11,10 +11,9 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.bumptech.glide.Glide;
 import com.example.petcarehome.homenavigation.Objetos.ReportePerdidasID;
 import com.example.petcarehome.R;
-import com.squareup.picasso.Callback;
-import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
 
@@ -45,17 +44,7 @@ public class AdapterReportesPerdidas extends RecyclerView.Adapter<AdapterReporte
         holder.etinombre.setText(listReportesPerdidas.get(position).getReportePerdidas().getNombre());
         holder.etidescripcion.setText(listReportesPerdidas.get(position).getReportePerdidas().getDescripcion());
         //holder.foto.setImageURI(Uri.parse(listReportesPerdidas.get(position).getFoto()));
-        Picasso.with(context).load(listReportesPerdidas.get(position).getReportePerdidas().getFoto()).into(holder.foto, new Callback() {
-            @Override
-            public void onSuccess() {
-                holder.foto.setVisibility(View.VISIBLE);
-            }
-
-            @Override
-            public void onError() {
-                Toast.makeText(context, "Error al cargar imagenes", Toast.LENGTH_LONG).show();
-            }
-        });
+        Glide.with(context).load(listReportesPerdidas.get(position).getReportePerdidas().getFoto()).into(holder.foto);
 
     }
 
