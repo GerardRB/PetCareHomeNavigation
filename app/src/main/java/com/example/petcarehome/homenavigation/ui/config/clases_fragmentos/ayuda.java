1427606dddb01df160643e0e334d78832f7d4d47
Uzo <uzouzo28@hotmail.com>
2021-01-03 21:00:56 -0600

    Apartados de configuración: -Ayuda -Cambiar contraseña (falta prueba) -Idioma

--- app/src/main/java/com/example/petcarehome/homenavigation/ui/config/clases_fragmentos/ayuda.java
+++ app/src/main/java/com/example/petcarehome/homenavigation/ui/config/clases_fragmentos/ayuda.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -11,11 +13,19 @@ import androidx.fragment.app.Fragment;
 
 import com.example.petcarehome.R;
 
+import org.w3c.dom.Text;
+
 public class ayuda extends Fragment {
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.ayuda_fragment, container,false);
+        return inflater.inflate(R.layout.ayuda_fragment, container, false);
         //Clase para el fragmento de "Ayuda"
+    }
+
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
     }
 }
--- app/src/main/java/com/example/petcarehome/homenavigation/ui/config/clases_fragmentos/idioma.java
+++ app/src/main/java/com/example/petcarehome/homenavigation/ui/config/clases_fragmentos/idioma.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -12,10 +14,23 @@ import androidx.fragment.app.Fragment;
 import com.example.petcarehome.R;
 
 public class idioma extends Fragment {
+    Button buttonid;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.idioma_fragment, container,false);
         //Clase para el fragmento de idioma
+
+
+    }
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        buttonid = view.findViewById(R.id.idioma_esp);
+        buttonid.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getContext(), "Espera nuevos idiomas :)", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
